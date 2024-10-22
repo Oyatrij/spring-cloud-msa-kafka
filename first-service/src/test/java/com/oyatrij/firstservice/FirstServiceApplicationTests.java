b9bc7b49204a9ba7450f82cddae86b8f1af5c94e
Oyatrij <oyatrij@gmail.com>
2024-10-22 15:11:33 +0900

    create: msa ê 구축 시작

---
+++ first-service/src/test/java/com/oyatrij/firstservice/FirstServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oyatrij.firstservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ second-service/src/main/java/com/oyatrij/secondservice/controller/SecondController.java
@@ -0,0 +1,28 @@
+package com.oyatrij.secondservice.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/second-service")
+@Slf4j
+public class SecondController {
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Welcome Second Service";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("second-request") String header){
+        log.info(header);
+        return "Hello Second Service";
+    }
+
+    @GetMapping("/check")
+    public String check(){
+        return "Check Second Service";
+    }
+}
